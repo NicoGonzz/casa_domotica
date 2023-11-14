b290146b7d8319e3889ae26044b98ea590594af1
NicoGonzz <juannicolas28@live.com>
2023-11-14 16:32:55 -0500

    Add real-time statistics using the Chart.js extension in the lights-component

--- src/app/components/luces/luces.component.ts
+++ src/app/components/luces/luces.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { environmet } from 'src/interface/config/environment';
+import Chart from 'chart.js/auto'; // Importa Chart.js
 
 interface LucesButton {
   id: number;
@@ -13,7 +14,7 @@ interface LucesButton {
   templateUrl: './luces.component.html',
   styleUrls: ['./luces.component.css']
 })
-export class LucesComponent {
+export class LucesComponent implements OnInit{
   estadisticasActivas = false;
   lucesButtons: LucesButton[] = [
     { id: 1, estado: 'OFF' },
@@ -24,9 +25,19 @@ export class LucesComponent {
   ];
   private tiempoEncendido: number[] = [0, 0, 0, 0, 0];
   private potenciaNominal = 50; // Puedes ajustar esto según la potencia nominal de tus luces
-
+// Datos para las gráficas
+  chartLabels: string[] = ['Cocina', 'Sala', 'Baño', 'Habitacion', 'Garaje'];
+  chartData: number[] = [10, 0, 100, 0, 1000];
+  chart: Chart | undefined;
 
   constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.createChart(); // Llama al método para crear la gráfica cuando se inicie el componente
+    setInterval(() => {
+      this.updateChartData(); // Actualiza los datos de la gráfica periódicamente
+    }, 1000)
+  }
 
   toggleEstadisticas() {
     this.estadisticasActivas = !this.estadisticasActivas;
@@ -88,6 +99,45 @@ export class LucesComponent {
       energiaTotal += this.calcularEnergia(i + 1);
     }
     return energiaTotal;
+  }
+  createChart() {
+    const canvas = document.getElementById('energyChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (ctx) {
+      this.chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: this.chartLabels,
+          datasets: [{
+            label: 'Consumo de energía por habitación',
+            data: this.chartData,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+  }
+  updateChartData() {
+    if (this.chart) {
+      // Actualiza los datos del gráfico con el cálculo de energía por habitación
+      for (let i = 0; i < this.chartData.length; i++) {
+        this.chartData[i] = this.calcularEnergia(i + 1);
+      }
+
+      // Actualiza los datos en el gráfico y lo renderiza
+      this.chart.data.datasets[0].data = this.chartData;
+      this.chart.update();
+    }
   }
 }
 
