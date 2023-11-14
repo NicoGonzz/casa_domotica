e9a2a35edbd17d1f140bc41764b3ead030ac2454
NicoGonzz <juannicolas28@live.com>
2023-11-14 00:04:47 -0500

    Enhance door component functionality

--- src/app/components/puertas/puertas.component.ts
+++ src/app/components/puertas/puertas.component.ts
@@ -17,6 +17,8 @@ export class PuertasComponent {
   puertaPrincipalEstado = 'CLOSE';
   puertaGarageEstado = 'CLOSE';
   vecesAbiertaPuertaPrincipal = 0;
+  ultimaAperturaPuertaPrincipal: string | null = null; // Guardará la hora de la última apertura
+  ultimaAperturaPuertaGarage: string | null = null;
 
   puertasButtons: PuertasButton[] = [
     { id: 6, estado: 'CLOSE' },
@@ -25,33 +27,11 @@ export class PuertasComponent {
 
   constructor(private http: HttpClient) {}
 
-  getBotonClase() {
-    return this.estadisticasActivas ? 'boton-activo' : 'boton-inactivo';
-  }
-
-  abrirPuertaPrincipal() {
-    this.puertaPrincipalEstado = 'Abierta';
-    this.vecesAbiertaPuertaPrincipal++;
-  }
-
-  cerrarPuertaPrincipal() {
-    this.puertaPrincipalEstado = 'Cerrada';
-  }
-
-  abrirPuertaGarage() {
-    this.puertaGarageEstado = 'Abierta';
-  }
-
-  cerrarPuertaGarage() {
-    this.puertaGarageEstado = 'Cerrada';
-  }
 
   private actualizarEstadoServidor(puertaId: number, nuevoEstado: string) {
     const url = `${environmet.url}/Orden/${puertaId}`;
-
     // Ajusta las propiedades del cuerpo según lo que espere tu backend
     let body: any;
-
     if (nuevoEstado === 'OPEN' || nuevoEstado === 'CLOSE') {
       // Ajusta las propiedades según lo que espere tu backend
       body = { StadoPuerta: nuevoEstado, Opendegrees: nuevoEstado === 'OPEN' ? 90 : 0, Closedegrees: 0 };
@@ -67,6 +47,10 @@ export class PuertasComponent {
           puerta.estado = nuevoEstado;
           if (puertaId === 6 && nuevoEstado === 'OPEN') {
             this.vecesAbiertaPuertaPrincipal++;
+            this.ultimaAperturaPuertaPrincipal = this.obtenerHoraActual();
+          } else if (puertaId === 7 && nuevoEstado === 'OPEN') {
+            //this.vecesAbiertaPuertaGarage++;
+            this.ultimaAperturaPuertaGarage = this.obtenerHoraActual();
           }
         }
       },
@@ -84,15 +68,32 @@ export class PuertasComponent {
     }
   }
 
-  getEstadoPuerta(puertaId: number): string {
-    const puerta = this.puertasButtons.find(puerta => puerta.id === puertaId);
-    return puerta?.estado || 'CLOSE';
+  getEstadoBoton(puertaId: number): string {
+    const puerta = this.puertasButtons.find(p => p.id === puertaId);
+
+    if (puerta) {
+      switch (puerta.estado) {
+        case 'OPEN':
+          return 'Abierta';
+        case 'CLOSE':
+          return 'Cerrada';
+        default:
+          return 'Estado Desconocido';
+      }
+    }
+
+    return 'Estado Desconocido';
   }
 
   getBotonColor(puertaId: number): string {
     const puerta = this.puertasButtons.find(puerta => puerta.id === puertaId);
-    return puerta?.estado === 'CLOSE' ? 'boton-rojo' : puerta?.estado === 'OPEN' ? 'boton-verde' : 'boton-azul';
-  }
-
+    return puerta?.estado === 'CLOSE' ? 'boton-rojo' : puerta?.estado === 'OPEN' ? 'boton-verde' : 'boton-azul';  }
+    private obtenerHoraActual(): string {
+      const ahora = new Date();
+      const horas = ahora.getHours().toString().padStart(2, '0');
+      const minutos = ahora.getMinutes().toString().padStart(2, '0');
+      const segundos = ahora.getSeconds().toString().padStart(2, '0');
+      return `${horas}:${minutos}:${segundos}`;
+    }
 }
 
