cc1744e4a103363fb75429f005662e94eda7723c
NicoGonzz <juannicolas28@live.com>
2023-11-15 14:17:12 -0500

    Add stadistics for the last component

--- src/app/components/control-estadisticas/control-estadisticas.component.ts
+++ src/app/components/control-estadisticas/control-estadisticas.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { environmet } from 'src/interface/config/environment';
+import Chart from 'chart.js/auto';
 
 interface LucesButton {
   id: number;
   nombreLed: string;
   TiempoUsoHoras: number;
   TiempoApagarHoras: number;
+  TiempoUsoSegundos: number;
+  TiempoApagarSegundos: number;
 }
 
 @Component({
@@ -17,12 +20,18 @@ interface LucesButton {
 })
 export class ControlEstadisticasComponent {
   lucesButtons: LucesButton[] = [
-    { id: 1, nombreLed: 'Led1', TiempoUsoHoras: 0, TiempoApagarHoras: 0 },
-    { id: 2, nombreLed: 'Led2', TiempoUsoHoras: 0, TiempoApagarHoras: 0 },
-    { id: 3, nombreLed: 'Led3', TiempoUsoHoras: 0, TiempoApagarHoras: 0 },
-    { id: 4, nombreLed: 'Led4', TiempoUsoHoras: 0, TiempoApagarHoras: 0 },
-    { id: 5, nombreLed: 'Led5', TiempoUsoHoras: 0, TiempoApagarHoras: 0 }
+    { id: 1, nombreLed: 'Energia Consumida en la cocina', TiempoUsoHoras: 0, TiempoApagarHoras: 0, TiempoUsoSegundos: 0, TiempoApagarSegundos: 0 },
+    { id: 2, nombreLed: 'Energia Consumida en la Sala', TiempoUsoHoras: 0, TiempoApagarHoras: 0, TiempoUsoSegundos: 0, TiempoApagarSegundos: 0 },
+    { id: 3, nombreLed: 'Energia Consumida en el Baño', TiempoUsoHoras: 0, TiempoApagarHoras: 0, TiempoUsoSegundos: 0, TiempoApagarSegundos: 0 },
+    { id: 4, nombreLed: 'Energia Consumida en la Habitacion', TiempoUsoHoras: 0, TiempoApagarHoras: 0, TiempoUsoSegundos: 0, TiempoApagarSegundos: 0 },
+    { id: 5, nombreLed: 'Energia Consumida en el Garaje', TiempoUsoHoras: 0, TiempoApagarHoras: 0, TiempoUsoSegundos: 0, TiempoApagarSegundos: 0 }
   ];
+
+  chartLabels: string[] = [];
+  chartData: number[] = [];
+
+  chart: Chart | undefined;
+
   constructor(private http: HttpClient) {}
 
   getDataFromServer() {
@@ -31,6 +40,16 @@ export class ControlEstadisticasComponent {
       (response) => {
         if (response.status === 'success') {
           this.lucesButtons = response.data;
+          this.lucesButtons = response.data.map((button: LucesButton) => ({
+            ...button,
+            TiempoUsoSegundos: +(button.TiempoUsoHoras * 3600).toFixed(2), // Convertir tiempo de uso a segundos
+            TiempoApagarSegundos: +(button.TiempoApagarHoras * 3600).toFixed(2),
+          }));
+
+          this.chartLabels = this.lucesButtons.map(button => button.nombreLed);
+          this.chartData = this.lucesButtons.map(button => button.TiempoUsoSegundos);
+          this.createChart();
+
           console.log(response.data);
         } else {
           console.error('Error al obtener datos del servidor');
@@ -41,4 +60,35 @@ export class ControlEstadisticasComponent {
       }
     );
   }
+
+  createChart() {
+    const canvas = document.getElementById('usageChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (ctx) {
+      this.chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: this.chartLabels,
+          datasets: [{
+            label: 'Tiempo de uso por LED (segundos)',
+            data: this.chartData,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+  }
+
 }
+
+
--- src/interface/config/environment.ts
+++ src/interface/config/environment.ts
@@ -1,3 +1,3 @@
 export const environmet = {
-  url: 'http://localhost:3000',
+  url: 'https://homeserves.onrender.com',
 }
